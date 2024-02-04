611d8ffe9b2efc24e2bb3cd5e810736f77cc4e65
Myke Bueno <myke159951@hotmail.com>
2024-02-04 12:35:00 -0300

    fished interface, att 0.1.1v

--- ping-a.py
+++ ping-a.py
@@ -1,25 +1,33 @@
 #/env/scripts python3
 
 """
-verifica o ping com uma lista de endereços IP
+verifica o ping com uma lista de endereços IP ("default_ping-a.txt")
+HP-M600-DESENVOLVIMENTO,192.168.17.28
 
 Script tenta gerar uma comunicação com os IPs definidos
 
 >>>ping-a.py
-        DEVICE      |       IP      |       ms       |      [12:49]
- printer_logistica  | 192.168.17.28 |       1       |
 
+------------------------------------------------------------------------------------------------
+|      DEVICE [12:49]       ||         IP          ||        PING        ||        Fail        |
+------------------------------------------------------------------------------------------------
+|  HP-M600-DESENVOLVIMENTO  ||   192.168.172.282   ||        off         ||       [0/5]        |
+|  HP-M600-DESENVOLVIMENTO  ||   192.168.172.282   ||        off         ||       [0/5]        |
+|  HP-M600-DESENVOLVIMENTO  ||   192.168.172.282   ||        off         ||       [0/5]        |
+|  HP-M600-DESENVOLVIMENTO  ||   192.168.172.282   ||        off         ||       [0/5]        |
+------------------------------------------------------------------------------------------------
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Myke Bueno"
-__licence__ = "Free"
+__licence__ = "Public Domain"
 
 from ping3 import ping as ping33
 from time import sleep
 from datetime import datetime
+import os
 
 
-
-
-# print(datetime.now())
+if __name__ == "__main__":
+    
+    pass
